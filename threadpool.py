b44b79229678ee9f2b986c9fb8ecd25f8d1c9080
George Christidis <ggg.xristidis@yahoo.gr>
2025-07-16 19:50:13 +0300

    feature: Implement threadpool module

---
+++ threadpool.py
@@ -0,0 +1,68 @@
+import threading
+from queue import Queue
+
+class Threadpool:
+    """Represents a group of working threads
+    
+    Attributes:
+        task_queue (Queue): a queue where the tasks to be executedare gathered
+        threads: the list of working threads
+        active (bool): a flag that terminates the threads if it is False
+        """
+    def __init__(self, num_threads):
+        self.task_queue: Queue = Queue()
+        self.active: bool = True
+        for _ in range(num_threads):
+            thread = threading.Thread(target=self.wait, args=())
+            thread.start()
+
+    def wait(self):
+        """Implements the wait state of each working thread
+        
+        When a task is inserted, a thread is assigned to execute it
+        """
+        while True:
+            if not(self.active):
+                break
+
+            if self.task_queue.empty():
+                continue
+        
+            current_task = self.task_queue.get()
+            [func, args, kwargs] = current_task
+            try:
+                func(*args, **kwargs)
+            except Exception as e:
+                print(e)
+
+    def add_task(self, func: "function", args=(), kwargs={}):
+        """Add a task to be executed
+        
+        Args:
+            func (function): the function to be executed
+            args (tuple): its arguments
+            kwargs (dict): its keyword arguments"""
+        task = (func, args, kwargs)
+        self.task_queue.put(task)
+
+    def terminate(self):
+        """Waits for the task queue to empty and then terminates the poolthread"""
+        while True:
+            if self.task_queue.empty():
+                break
+        self.active = False
+
+if __name__ == "__main__":
+    threadpool = Threadpool(4)
+    
+    i = 0
+    while True:
+        threadpool.add_task(print, (i, ))
+        if i > 50:
+            threadpool.terminate()
+            break
+        i += 1
+    threadpool.add_task(print, (1, ))
+    
+
+
